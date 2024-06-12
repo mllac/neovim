90715a76ae7e14565d2e8e4cda42cea9af692e9a
= <=>
2024-06-12 15:17:36 -0500

    add cool thingies

--- config/options.nix
+++ config/options.nix
@@ -8,22 +8,27 @@
     # TAB // INDENT
     
     smartindent = true;
+    signcolumn = "yes";
     ignorecase = true;
+    swapfile = false;
     smartcase = true;
     expandtab = true;
     softtabstop = 2;
     shiftwidth = 2;
     wrap = false;
     tabstop = 2;
+    list = true;
 
     # LIST CHARS
 
     listchars = {
       precedes = "«";
       extends = "»";
+      space = "⋅";
       trail = "·";
       nbsp = "_";
       tab = "▸ ";
+      eol = "↴";
     };
   };
 }
