388fa77582e287b2ebedf29c4e17f9c2c31de88b
= <=>
2024-06-11 21:48:42 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,46 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs?ref=nixos-unstable";
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { nixvim, flake-parts, ... }@inputs:
+  
+  flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [
+      "x86_64-linux"
+    ];
 
+    perSystem = { pkgs, system, ... }: 
+    let
+      nixvim' = nixvim.legacyPackages.${system};
+      nixvimLib = nixvim.lib.${system};
+
+      nixvimModule = {
+        inherit pkgs;
+
+        module = import ./config;
+      };
+
+      nvim = nixvim'.makeNixvimWithModule nixvimModule;
+    in {
+      checks = {
+        default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+      };
+
+      packages = {
+        default = nvim;
+      };
+    };
   };
 }
