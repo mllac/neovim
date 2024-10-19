8e8890c994ab60d2791205d9b4ce8c8a3f9a1a7e
= <=>
2024-10-19 04:48:37 -0500

    resort

--- flake.nix
+++ flake.nix
@@ -15,11 +15,24 @@
   let
     android = "aarch64-linux";
 
+    system = "x86_64-linux";
+
+    pkgs
+      = nixpkgs.legacyPackages.${system};
     a_pkgs
-    = nixpkgs.legacyPackages.${android};
+      = nixpkgs.legacyPackages.${android};
 
     a_nixvim'
-    = nixvim.legacyPackages.${android};
+      = nixvim.legacyPackages.${android};
+
+    nixvim'
+      = nixvim.legacyPackages.${system};
+    
+    mod = {
+      module = import ./config;
+
+      inherit pkgs;
+    };
 
     a_mod = {
       module = import ./config;
@@ -29,19 +42,6 @@
 
     a_vim =
       a_nixvim'.makeNixvimWithModule a_mod;
-
-    system = "x86_64-linux";
-
-    pkgs
-      = nixpkgs.legacyPackages.${system};
-    nixvim'
-      = nixvim.legacyPackages.${system};
-    
-    mod = {
-      module = import ./config;
-
-      inherit pkgs;
-    };
 
     vim =
       nixvim'.makeNixvimWithModule mod;
